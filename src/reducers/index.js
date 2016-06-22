1c3a7e7651e8e89b87ce8f5333e0002a99d9abe6
Cassie Koomjian <schmitzc@gmail.com>
2016-06-22 09:42:36 -0700

    Show book details for selected book

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducers_books';
+import BooksReducer from './reducer_books';
+import ActveBookReducer from './reducer_active_book'
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActveBookReducer
 });
 
 export default rootReducer;
